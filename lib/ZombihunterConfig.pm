ac0d96671016d782c61267b79a8ab7a2475fcc4e
Volker Thomas <volker.thomas@de.abb.com>
2017-12-10 15:21:41 +0100

    First commit of the package which handles the configuration settings of Zombihunter.

---
+++ lib/ZombihunterConfig.pm
@@ -0,0 +1,46 @@
+package lib::ZombihunterConfig;
+
+#-------------------------------------------------------------------------------
+#   This package covers creation and handling of program environment like
+#   reading the config file and creating target folders etd.
+#-------------------------------------------------------------------------------
+
+use strict;
+use warnings;
+use 5.24.0;
+
+require Exporter;
+
+our @ISA = qw ( Exporter );
+
+our @EXPORT = qw (
+	read_config
+	);
+
+#-------------------------------------------------------------------------------
+#   Reads the Zombihunter config file and returns the result in a hash of arrays
+#
+sub read_config {
+	my $config_path = './config/zombihunter.conf';
+
+	my %config;
+
+	open CONFIG, '<', $config_path or
+		die "Couldn't read config file $config_path: $!";
+
+	while (<CONFIG>) {
+		my $line = $_;
+		chomp $line;
+		next if $line =~ /^#/;      # ignore comments
+		next if $line =~ /^\s*?$/;  # ignore empty lines
+
+		my ($key, $settings) = split '\|', $line;
+		my @values = split ';', $settings;
+		$config{$key} = \@values;
+	}
+	close CONFIG;
+
+	return \%config;
+}
+
+1;
