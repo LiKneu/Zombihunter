563dedefceae2ef43ab6aa1080e1a4e016c43d98
Li Kneu <git@vthomas.de>
2017-12-20 06:42:21 +0100

    Added option to reorganize the selected folder i.e. removing double files and replace them with shortcuts.

--- main.pl
+++ main.pl
@@ -8,11 +8,13 @@ use strict;
 use warnings;
 use 5.24.0;
 
-my $VERSION = '18.12.2017';
+my $VERSION = '20.12.2017';
 
 use lib::ZombihunterConfig;
 use lib::ZombihunterUI;
 use lib::ScanFolders;
+use lib::EvaluateData;
+use lib::OrganizeData;
 
 my $settings = read_config;
 
@@ -20,21 +22,28 @@ initiate_console( $VERSION );
 
 my $choice = '';
 
-until ( $choice =~ 'q' ){
+until ( $choice =~ 'q' ){           # exit program
 	$choice = show_main_menu();
 
-	if ( $choice =~ 'h') {
+	if ( $choice =~ 'h') {          # help
 		show_help();
 	}
-	elsif ( $choice =~ '1' ) {
+	elsif ( $choice =~ '1' ) {      # edit settings
 		$choice = edit_config( $settings );
 		$settings = read_config();
 	}
-	elsif ( $choice =~ '2' ) {
+	elsif ( $choice =~ '2' ) {      # scan folder
 		if ( show_scan_folder( $settings->{ScanFolder} ) ) {
 			show_scanning();
 			scan_folders( $settings->{ScanFolder} );
 		}
+	}
+	elsif ( $choice =~ '3' ) {      # reorganize folder
+
+		# TODO: allow user to select the scanfile dynamically
+		my $data = read_log('./scans/zh_log_2017-12-18_6_53_15.txt');
+
+		loop_data( $data, $settings->{CommonFolder} );
 	}
 	else {
 		exit;
